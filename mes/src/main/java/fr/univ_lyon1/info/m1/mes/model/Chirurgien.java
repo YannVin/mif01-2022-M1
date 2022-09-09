5e193afc2827d5e3010fe205ff9e6be6cb147f3d
VINCENT YANN p1906701 <yann.vincent@etu.univ-lyon1.fr>
2022-09-09 07:21:46 +0000

    Upload New File

---
+++ mes/src/main/java/fr/univ_lyon1/info/m1/mes/model/Chirurgien.java
@@ -0,0 +1,7 @@
+package fr.univ_lyon1.info.m1.mes.model;
+
+public class Chirurgien extends HealthProfessional {
+    public Chirurgien(final String name, final MES mes) {
+        super(name, mes);
+    }
+}
